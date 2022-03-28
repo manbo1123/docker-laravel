2f71128734a1ec982182a0994f81dd5e753bfc71
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2022-03-28 10:07:06 +0900

    create_dummy_datas

---
+++ src/database/migrations/2022_03_18_102644_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('カテゴリー名');
+            $table->unsignedBigInteger('parent_id');
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('parent_cats')->comment('親カテゴリーid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
